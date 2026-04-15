c47b8307c154d5f708c5b4d765bdae8e8a8d151c
gurusupriya <152900966+gurusupriya@users.noreply.github.com>
2026-04-15 19:28:45 +0530

    Update gemini_utils.py

--- gemini_utils.py
+++ gemini_utils.py
@@ -1,21 +1,20 @@
 from groq import Groq
 import json
 import re
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
 def call_groq(prompt):
+    api_key = st.session_state.get("GROQ_API_KEY")
+
+    client = Groq(api_key=api_key)
+
     response = client.chat.completions.create(
-        model="llama-3.3-70b-versatile",  # fast + powerful
+        model="llama-3.3-70b-versatile",
         messages=[
             {"role": "user", "content": prompt}
         ],
         temperature=0.7
     )
+
     return response.choices[0].message.content
 
 
