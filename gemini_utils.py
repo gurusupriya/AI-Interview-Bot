179859c75b060d43cc84c163430819f2818b492e
gurusupriya <152900966+gurusupriya@users.noreply.github.com>
2026-04-15 19:10:16 +0530

    Update gemini_utils.py

--- gemini_utils.py
+++ gemini_utils.py
@@ -1,17 +1,23 @@
-import google.generativeai as genai
+from groq import Groq
 import json
 import re
 import os
 from dotenv import load_dotenv
 
 load_dotenv()
-genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 
+client = Groq(api_key=os.getenv("GROQ_API_KEY"))
 
-def call_gemini(prompt):
-    model = genai.GenerativeModel("gemini-2.5-flash")
-    response = model.generate_content(prompt)
-    return response.text
+def call_groq(prompt):
+    response = client.chat.completions.create(
+        model="llama-3.3-70b-versatile",  # fast + powerful
+        messages=[
+            {"role": "user", "content": prompt}
+        ],
+        temperature=0.7
+    )
+    return response.choices[0].message.content
+
 
 def extract_json(text):
     match = re.search(r"\{.*\}", text, re.DOTALL)
